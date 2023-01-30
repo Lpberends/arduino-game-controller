89a9470905fc3a385c0c8813903516d20750e20d
Lpberends <91067257+Lpberends@users.noreply.github.com>
2023-01-30 09:20:25 +0100

    Add files via upload

---
+++ sketch_nov21c.ino
@@ -0,0 +1,24 @@
+#include <Keyboard.h>
+
+const int LeftButton = 11;
+const int RightButton = 12;
+void setup(){
+   pinMode(LeftButton, INPUT);
+   pinMode(RightButton, INPUT);
+   Keyboard.begin();
+} 
+void loop()
+{   
+  if(digitalRead(LeftButton) == HIGH){
+  Keyboard.press(97);
+  delay(100);
+   }
+if(digitalRead(RightButton) == HIGH){
+  Keyboard.press(100);
+  delay(100);
+   }
+if(digitalRead(11) == LOW){
+if(digitalRead(12) == LOW)
+  Keyboard.releaseAll();
+   }
+}
